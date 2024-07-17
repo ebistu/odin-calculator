51ce84eac9fcbf9fe79a44ad850c799addd41a2b
ebistu <127283670+ebistu@users.noreply.github.com>
2024-07-18 02:40:53 +0300

    did ui and logic

--- app.js
+++ app.js
@@ -2,17 +2,77 @@
 let firstNumber = 0; 
 let secondNumber = 0; 
 let operator = "";
+let displayValue = "";
+
+const calcBtns = document.querySelectorAll('.calc-button');
+const operBtns = document.querySelectorAll('.operator-button');
+
+calcBtns.forEach(button => {
+    button.addEventListener('click', function(event) {
+        updateDisplay(event.target.id);
+    });
+});
+
+operBtns.forEach(button => {
+    button.addEventListener('click', function(event) {
+        switch(event.target.id){
+            case "c":
+                updateDisplay("");
+                break;
+            case "=":
+                parseDisplayValue();
+                console.log(displayValue);
+                console.log(firstNumber);
+                console.log(operator);
+                //updateDisplay("");
+                //updateDisplay(operate(operator, firstNumber, secondNumber));
+                break;
+            case "c":
+                updateDisplay("");
+                break;
+            default:
+                updateDisplay(event.target.id);
+                break;
+        }
+    });
+});
+
+function parseDisplayValue(){
+    for(i=0;i<displayValue.length;i++){
+        switch(displayValue.charAt(i)){
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                operator = displayValue.charAt(i);
+                console.log(displayValue.)
+                
+                break;
+        }
+    }
+}
+
+function updateDisplay(txt){
+    const display = document.querySelector(".display");
+    if(txt === ""){
+        display.textContent = "";
+    }
+    if(display.textContent.length < 24){
+        display.textContent += txt;
+    }
+    displayValue = display.textContent;
+}
 
 function operate(operator, firstNum, secondNum){
     switch(operator){
         case "+":
-            add(firstNum, secondNum);
+            return add(firstNum, secondNum);
         case "-":
-            subtract(firstNum, secondNum);
+            return subtract(firstNum, secondNum);
         case "*":
-            multiply(firstNum, secondNum);
+            return multiply(firstNum, secondNum);
         case "/":
-            divide(firstNum, secondNum);
+            return divide(firstNum, secondNum);
     }
 }
 
